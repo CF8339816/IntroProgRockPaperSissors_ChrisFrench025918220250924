793b49b9fbac34515d3f0468fea3d3e9e658c950
CF8339816 <chris_french_3dga@hotmail.com>
2025-09-24 15:05:03 -0300

    prog update seems to work
    not sure if random for challenger choice is working

--- IntroProgRockPaperSissors_ChrisFrench025918220250924/Program.cs
+++ IntroProgRockPaperSissors_ChrisFrench025918220250924/Program.cs
@@ -10,20 +10,41 @@ namespace IntroProgRockPaperSissors_ChrisFrench025918220250924
 {
     internal class Program
     {
-
+        static int Wins = 0;
+        static int Loose = 0;
         static Random random = new Random();
       
         static void Main(string[] args)
         {
+            Console.WriteLine("Lets play Rock, Paper, Sissors, best 3 out of 5 rounds! ");
+            Console.ReadKey(true);
 
            
-            WANA();
+             WANA();
 
 
         }
         //Methods below here
+
+
+        static void winOrLoose()
+        {
+            if (Wins == 3)
+            {
+                Console.WriteLine($"You have {Wins} wins. Good Job you are the winner this turnament");
+                Console.ReadKey(true);
+            }
+            else if (Loose == 3)
+            {
+                Console.WriteLine($"You have {Loose} losses. Too bad , this time you are the looser this turnament");
+                Console.ReadKey(true);
+               
+            }
+
+        }
         static void WANA()
         {
+            Console.WriteLine($"You have {Wins} wins and {Loose} losses" + "\n");
             Console.WriteLine("Do you wan to play a round? y = yes, n= no");
             string playAround = Console.ReadLine();
 
@@ -42,82 +63,89 @@ namespace IntroProgRockPaperSissors_ChrisFrench025918220250924
         static void PlayGame()
         {
             
-                int ChallengerThrow = random.Next(1, 4);
+            int ChallengerThrow = random.Next(1, 4);
 
-                Console.WriteLine("what is your Throw?  1 = Rock, 2 = Paper, 3 = Sissors");
-                string playerThrow = Console.ReadLine();
+            Console.WriteLine("what is your Throw?  1 = Rock, 2 = Paper, 3 = Sissors");
+            string playerThrow = Console.ReadLine();
 
-                if (playerThrow == "1")
+            if (playerThrow == "1")
+            {
+                if (ChallengerThrow == 2)
                 {
-                    if (ChallengerThrow == 2)
-                    {
-                        Console.WriteLine("You chose Rock, the challenger chose Paper...You loose..");
-                    Console.ReadKey(true);
+                    Console.WriteLine("You chose Rock, the challenger chose Paper...You loose..");
+                Console.ReadKey(true);
+                Loose ++;
                 }
-                    else if (ChallengerThrow == 3)
-                    {
-                        Console.WriteLine("You chose Rock, the challenger chose Sissors...You Win..");
-                    Console.ReadKey(true);
-                }
-                    else
-                    {
-                        Console.WriteLine("You chose Rock, the challenger chose Rock...great minds think alike \n It is a tie....");
-                    Console.ReadKey(true);
-                }
-                WANA();
-                }
-
-                if (playerThrow == "2")
+                else if (ChallengerThrow == 3)
                 {
-                    if (ChallengerThrow == 1)
-                    {
-                        Console.WriteLine("You chose Paper, the challenger chose Rock...You Win..");
-                    Console.ReadKey(true);
+                    Console.WriteLine("You chose Rock, the challenger chose Sissors...You Win..");
+                Console.ReadKey(true);
+                Wins++;
                 }
-                    else if (ChallengerThrow == 3)
-                    {
-                        Console.WriteLine("You chose Paper, the challenger chose Sissors...You Loose..");
-                    Console.ReadKey(true);
-                }
-                    else
-                    {
-                        Console.WriteLine("You chose Paper, the challenger chose Paper...great minds think alike \n It is a tie....");
-                    Console.ReadKey(true);
-                }
-                WANA();
-                }
-
-                if (playerThrow == "3")
+                else
                 {
-                    if (ChallengerThrow == 1)
-                    {
-                        Console.WriteLine("You chose Sissors, the challenger chose Rock...You Loose..");
-                    Console.ReadKey(true);
+                    Console.WriteLine("You chose Rock, the challenger chose Rock...great minds think alike \n It is a tie....");
+                Console.ReadKey(true);
                 }
-                    else if (ChallengerThrow == 2)
-                    {
-                        Console.WriteLine("You chose Sissors, the challenger chose Paper...You Win..");
-                    Console.ReadKey(true);
-                }
-                    else
-                    {
-                        Console.WriteLine("You chose Sissors, the challenger chose Sissors...great minds think alike \n It is a tie....");
-                    Console.ReadKey(true);
-                }
-
-                WANA();
-                }
-
-               
-
-
-
+                
             }
+
+            if (playerThrow == "2")
+            {
+                if (ChallengerThrow == 1)
+                {
+                    Console.WriteLine("You chose Paper, the challenger chose Rock...You Win..");
+                Console.ReadKey(true);
+                Wins++;
+                }
+                else if (ChallengerThrow == 3)
+                {
+                    Console.WriteLine("You chose Paper, the challenger chose Sissors...You Loose..");
+                Console.ReadKey(true);
+                Loose ++;
+                }
+                else
+                {
+                    Console.WriteLine("You chose Paper, the challenger chose Paper...great minds think alike \n It is a tie....");
+                Console.ReadKey(true);
+                }
+                
+            }
+
+            if (playerThrow == "3")
+            {
+                if (ChallengerThrow == 1)
+                {
+                    Console.WriteLine("You chose Sissors, the challenger chose Rock...You Loose..");
+                Console.ReadKey(true);
+                Loose++;
+                }
+                else if (ChallengerThrow == 2)
+                {
+                    Console.WriteLine("You chose Sissors, the challenger chose Paper...You Win..");
+                Console.ReadKey(true);
+                Wins++;
+                }
+                else
+                {
+                    Console.WriteLine("You chose Sissors, the challenger chose Sissors...great minds think alike \n It is a tie....");
+                Console.ReadKey(true);
+                }
+
+             
+            }
+            winOrLoose();
+            WANA();
+
+
+
 
         }
 
-
-
-
     }
+
+
+
+
+}
 
